fe3e2cf5b3d2c572db7bcf72cdc814d851f9ea78
MK <kolodziejczykmichal269@gmail.com>
2021-10-06 14:46:20 +0200

    main-navigation RWD

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -11,7 +11,6 @@ import CartContext from "../../store/cart-context";
 import logo from "../../public/logo.svg";
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
   {
     name: "Carabiners",
     href: "/products/carabiners",
@@ -65,7 +64,7 @@ function MainNavigation() {
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -76,7 +75,7 @@ function MainNavigation() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
                 <Link href="/">
                   <a className="flex-shrink-0 flex items-center">
                     <Image
@@ -86,12 +85,12 @@ function MainNavigation() {
                       height="36"
                       className="fill-current text-green-600"
                     />
-                    <p className="text-gray-300 text-xl ml-2 font-medium">
+                    <p className="hidden sm:block md:hidden lg:block text-gray-300 text-xl ml-2 font-medium">
                       Climbing Shop
                     </p>
                   </a>
                 </Link>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden md:block md:ml-6">
                   <div className="flex space-x-4">
                     {navigationState.map((item) => (
                       <Link href={item.href} key={item.name}>
@@ -193,7 +192,7 @@ function MainNavigation() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
                 <a
