1e3dd90a6447e1abf3e6ca76259573c32bf0f90a
MK <kolodziejczykmichal269@gmail.com>
2021-10-13 14:49:57 +0200

    Update Cart page

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -15,24 +15,35 @@ function CartPage() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="mx-auto py-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       {cart.length === 0 && <h1 className="text-3xl">Your cart is empty</h1>}
       {cart && cart.length > 0 && (
         <>
-          <h1 className="text-3xl">Cart Page</h1>
-          <div className="mt-5">
+          <h1 className="text-3xl text-center">Your Cart</h1>
+          <div className="mt-5 mb-5 pb-10 border-b-2">
             <ul className="grid grid-cols-12 border-b-2">
-              <li className="col-span-7">Name</li>
-              <li className="col-span-2">Brand</li>
-              <li className="col-span-2">Price</li>
+              <li className="col-span-8 md:col-span-7 font-semibold hidden sm:block">
+                Product
+              </li>
+              <li className="col-span-4 md:col-span-2 font-semibold hidden sm:block">
+                Brand
+              </li>
+              <li className="col-span-2 font-semibold hidden md:block">
+                Price
+              </li>
             </ul>
             <CartList cart={cart} />
-            <buton onClick={clearCartHandler}>Clear cart</buton>
+            <button
+              className="flex mx-auto md:mx-0 text-white bg-gray-800 border-0 py-2 px-8 focus:outline-none hover:bg-gray-700 rounded mt-5"
+              onClick={clearCartHandler}
+            >
+              Clear cart
+            </button>
           </div>
           <CartForm
             cart={cart}
             createOrderInfo={cartCtx.createOrderInformation}
-            onSubmit={() => router.push("/checkout")}
+            pushToCheckout={() => router.push("/checkout")}
           />
         </>
       )}
