2a042b7beb869ab6b56b839474e71de764518cf9
MK <kolodziejczykmichal269@gmail.com>
2021-10-14 15:06:53 +0200

    Updata Layout and main-navigation

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
+import Head from "next/head";
 
 import MainNavigation from "./main-navigation";
 
 function Layout(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Climbing Shop</title>
+      </Head>
       <MainNavigation />
       <main>{props.children}</main>
     </Fragment>
