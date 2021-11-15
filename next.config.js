2339c3a33a5affb8ca739b74ab8f64145bf5fd37
MK <kolodziejczykmichal269@gmail.com>
2021-11-15 13:02:27 +0100

    Add AWS S3 domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["strapi-nextjs-shop.herokuapp.com", "localhost", ""],
+    domains: [
+      "next-strapi269.s3.eu-central-1.amazonaws.com",
+      "strapi-nextjs-shop.herokuapp.com",
+      "localhost",
+      "",
+    ],
   },
 };
